1aa85a42fb35a642cfa3c13049648739ea351578
lelkaklel <lelkaklel@gmail.com>
2014-07-04 01:33:59 +0400

    update

--- d3js_examples/level_5/example.js
+++ d3js_examples/level_5/example.js
@@ -1,24 +1,39 @@
 
-// массив данных в формате JSON
+function visualize(data) {
 
-json_data = [
-    {"width": 10},
-    {"width": 40},
-    {"width": 100},
-    {"width": 190},
-    {"width": 170},
-    {"width": 110},
-    {"width": 80},
-    {"width": 30},
-    {"width": 20},
-]
+    // создаем график
+
+    var body = d3.select("body").append("div");
+
+    var divs = body  // выбираем селектор body
+        .selectAll("div")  // просто так нужно =)
+        .data(data)  // привязываем данные
+        .enter()  // входим в цикл по данным
+        .append("div")  // создаем элемент div для каждого элемента данных
+        .attr("class", "div_bar")
+        .style("width", function(d){ return d.boobs_size*100 + 'px';});  // меняем его ширину пропорционально текущему значению данных
+
+    // добавим названия столбцов
+
+    divs.append("div")
+        .attr("class", "div_index")
+        .text( function(d,i){ return d.name; } );  // функции для элементов, к которым привязаны данные принимают 2 значения,
+                                              // d - элемент массива данных и i - порядковый элемент элемента данных
+                                              // в массиве
+
+    // добавим надписи со значениями
+
+    divs.append("div")
+        .attr("class", "div_value")
+        .text( function(d){ return d.boobs_size; } );
+
+}
+
+d3.json("data.json",
+    function (error, data) {
+        if (error) return console.warn(error);
+        visualize(data);
+    });
 
 // строим график из простых div-ов на основе данных
 
-d3.select("body")  // выбираем селектор body
-    .data(json_data)  // привязываем данные
-    .enter()  // входим в цикл по данным
-        .append("div")  // создаем элемент div для каждого элемента данных
-        .attr("class", "chart_div")
-        .style("width", function(d){ return d.width*3 + 'px';})  // меняем его ширину пропорционально текущему значению данных
-        .text(function(d){ return d.width;});  // то же самое с текстом
